ada4a17c1f3e546284af5876cac9d718a8e53c71
Omer Kaya <omerkaya513@gmail.com>
2019-07-06 18:02:51 +0300

    Working version of the programme. Needs testing!

--- taskmanager.go
+++ taskmanager.go
@@ -8,16 +8,20 @@ import (
 
 var (
 	taskSlice = []func() error{func() error {
-		//time.Sleep(time.Second)
+		time.Sleep(time.Second)
 		fmt.Println("First finished")
 		return nil
 	}, func() error {
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second)
 		fmt.Println("Second finished")
 		return nil
 	}, func() error {
-		time.Sleep(time.Second * 2)
-		fmt.Println("Second and half finished")
+		time.Sleep(time.Second)
+		fmt.Println("Third finished")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second)
+		fmt.Println("Third and half finished")
 		return nil
 	}, func() error {
 		time.Sleep(time.Second * 3)
@@ -30,7 +34,8 @@ var (
 	}, func() error {
 		time.Sleep(time.Second * 4)
 		fmt.Println("Fifth, with the error")
-		return fmt.Errorf("error")
+		//return fmt.Errorf("error")
+		return nil
 	}, func() error {
 		time.Sleep(time.Second * 10)
 		fmt.Println("Final, with the error")
@@ -38,66 +43,50 @@ var (
 	}}
 )
 
-var wg sync.WaitGroup
-
 // We need to use channels and wait groups!
-func DoCool(tasks []func() error, perTime int, errLimit int) {
+func DoCool(tasks []func() error, perTime, errLimit int) {
 	if perTime <= 0 {
 		return
 	}
-
+	wg := sync.WaitGroup{}
 	taskChan := make(chan func() error, perTime)
-	errChan := make(chan error)
-	stop := make(chan struct{})
+	errChan := make(chan error, perTime)
 
-	go fillTasks(tasks, taskChan)
+	go func(taskSlice []func() error, errCh chan func() error) {
+		for _, t := range taskSlice {
+			errCh <- t
+		}
+	}(tasks, taskChan)
 
-	wg.Add(1)
-	go errs(taskChan, errChan)
-
-	// Main routine
-Label:
+LABEL:
 	for {
 		select {
-		case logErr := <-errChan:
-			fmt.Printf("Error: %#v; Left: %d\n", logErr, errLimit)
-			if logErr != nil {
-				fmt.Printf("Error: %#v; Left: %d\n", logErr, errLimit)
+		case t := <-taskChan:
+			wg.Add(1)
+			go func(task func() error, errCh chan error) {
+				defer wg.Done()
+				errCh <- t()
+			}(t, errChan)
+			break
+		case e := <-errChan:
+			if e != nil {
+				fmt.Printf(" error: %v\n", e)
 				errLimit--
 			}
+			fmt.Printf("Limit: %d\n", errLimit)
 			if errLimit == 0 {
-				fmt.Printf("Or here?\n")
-				stop <- struct{}{}
-				break Label
+				fmt.Printf("reached the limit!\n")
+				close(taskChan)
+				close(errChan)
+				break LABEL
 			}
-		case <-stop:
-			break Label
+			break
 		}
 	}
-	fmt.Printf("Done!\n")
-	wg.Done()
 	return
-}
-
-func fillTasks(innerTasks []func() error, taskChannel chan func() error) {
-	for _, t := range innerTasks {
-		taskChannel <- t
-	}
-	close(taskChannel)
-	return
-}
-
-func errs(tasks chan func() error, errors chan error) {
-	for t := range tasks {
-		errors <- t()
-	}
-	wg.Done()
-	close(errors)
 }
 
 func main() {
-	wg.Add(1)
-	go DoCool(taskSlice, 2, 1)
-	wg.Wait()
+	DoCool(taskSlice, 4, 1)
 	return
 }
