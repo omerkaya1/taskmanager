33eb217d4cb1895dd2248bc2a50d23b0721b6506
Omer Kaya <omerkaya513@gmail.com>
2019-07-07 00:32:59 +0300

    The solution for the issue of not closing the created channels:
    1) The task channel is closed by the range statement;
    2) The error channel gets closed when all the tasks are
    enqueued.

--- taskmanager.go
+++ taskmanager.go
@@ -1,92 +1,44 @@
-package main
+package taskmanager
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-var (
-	taskSlice = []func() error{func() error {
-		time.Sleep(time.Second)
-		fmt.Println("First finished")
-		return nil
-	}, func() error {
-		time.Sleep(time.Second)
-		fmt.Println("Second finished")
-		return nil
-	}, func() error {
-		time.Sleep(time.Second)
-		fmt.Println("Third finished")
-		return nil
-	}, func() error {
-		time.Sleep(time.Second)
-		fmt.Println("Third and half finished")
-		return nil
-	}, func() error {
-		time.Sleep(time.Second * 3)
-		fmt.Println("Third finished")
-		return nil
-	}, func() error {
-		time.Sleep(time.Second * 5)
-		fmt.Println("Fourth finished")
-		return nil
-	}, func() error {
-		time.Sleep(time.Second * 4)
-		fmt.Println("Fifth, with the error")
-		//return fmt.Errorf("error")
-		return nil
-	}, func() error {
-		time.Sleep(time.Second * 10)
-		fmt.Println("Final, with the error")
-		return fmt.Errorf("error")
-	}}
-)
-
-// We need to use channels and wait groups!
-func DoCool(tasks []func() error, perTime, errLimit int) {
-	if perTime <= 0 {
+func Worker(tasks []func() error, perTime, errLimit int, report chan<- int) {
+	if perTime <= 0 || len(tasks) == 0 {
 		return
 	}
-	wg := sync.WaitGroup{}
+
 	taskChan := make(chan func() error, perTime)
-	errChan := make(chan error, perTime)
+	errChan := make(chan error)
 
-	go func(taskSlice []func() error, errCh chan func() error) {
-		for _, t := range taskSlice {
-			errCh <- t
+	go enqueue(tasks, taskChan)
+	go manager(taskChan, errChan)
+
+	// The loop is used to handle cases when we either don't have errors or
+	// the allowed number of errors is equal to the number of
+	// errors encountered during the execution
+	for i := 0; i < len(tasks); i++ {
+		if e := <-errChan; e != nil {
+			report <- errLimit
+			errLimit--
 		}
-	}(tasks, taskChan)
-
-LABEL:
-	for {
-		select {
-		case t := <-taskChan:
-			wg.Add(1)
-			go func(task func() error, errCh chan error) {
-				defer wg.Done()
-				errCh <- t()
-			}(t, errChan)
-			break
-		case e := <-errChan:
-			if e != nil {
-				fmt.Printf(" error: %v\n", e)
-				errLimit--
-			}
-			fmt.Printf("Limit: %d\n", errLimit)
-			if errLimit == 0 {
-				fmt.Printf("reached the limit!\n")
-				close(taskChan)
-				close(errChan)
-				break LABEL
-			}
-			break
+		if errLimit <= 0 {
+			return
 		}
 	}
-	return
 }
 
-func main() {
-	DoCool(taskSlice, 4, 1)
-	return
+func enqueue(taskSlice []func() error, taskCh chan func() error) {
+	for _, t := range taskSlice {
+		taskCh <- t
+	}
+}
+
+func manager(tasks chan func() error, errCh chan error) {
+	defer close(errCh)
+	// Do the tasks concurrently; the range will take care of the taskChan closure
+	for t := range tasks {
+		go func(task func() error, errChannel chan error) {
+			if e := task(); e != nil {
+				errChannel <- e
+			}
+		}(t, errCh)
+	}
 }
--- taskmanager_test.go
+++ taskmanager_test.go
@@ -1,41 +1,66 @@
-package main
+package taskmanager
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
-//var (
-//	taskSlice = []func() error { func() error {
-//		//time.Sleep(time.Second)
-//		fmt.Println("First finished")
-//		return nil
-//	}, func() error {
-//		time.Sleep(time.Second * 3)
-//		fmt.Println("Second finished")
-//		return nil
-//	}, func() error {
-//		time.Sleep(time.Second * 2)
-//		fmt.Println("Second and half finished")
-//		return nil
-//	}, func() error {
-//		time.Sleep(time.Second * 3)
-//		fmt.Println("Third finished")
-//		return nil
-//	}, func() error {
-//		time.Sleep(time.Second * 5)
-//		fmt.Println("Fourth finished")
-//		return nil
-//	}, func() error {
-//		time.Sleep(time.Second * 4)
-//		fmt.Println("Fifth, with the error")
-//		return fmt.Errorf("error")
-//	}, func() error {
-//		time.Sleep(time.Second * 10)
-//		fmt.Println("Final, with the error")
-//		return fmt.Errorf("error")
-//	}}
-//)
+var (
+	taskSlice = []func() error{func() error {
+		time.Sleep(time.Second)
+		fmt.Println("First finished")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second)
+		fmt.Println("Second finished")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second)
+		fmt.Println("Third finished")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second * 3)
+		fmt.Println("Fourth finished")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second * 6)
+		fmt.Println("Fifth finished")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second * 5)
+		fmt.Println("Sixth finished with an error")
+		return fmt.Errorf("fourth error")
+	}, func() error {
+		time.Sleep(time.Second * 7)
+		fmt.Println("Seventh finished with an error")
+		return fmt.Errorf("fifth error")
+	}, func() error {
+		time.Sleep(time.Second * 8)
+		fmt.Println("Semi-final")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second * 10)
+		fmt.Println("Almost final, with an error")
+		return nil
+	}, func() error {
+		time.Sleep(time.Second * 10)
+		fmt.Println("Final, with an error")
+		return fmt.Errorf("yet another error")
+	}}
+)
 
-func TestTaskManager_DoCool(t *testing.T) {
-	DoCool(taskSlice, 3, 1)
+func TestWorker_ZeroErrorTolerance(t *testing.T) {
+	errNum := 0
+	test := make(chan int, errNum)
+	Worker(taskSlice[4:], 5, 0, test)
+	assert.NoError(t, nil)
+
+}
+
+func TestWorker_OneErrorTolerance(t *testing.T) {
+	errNum := 2
+	test := make(chan int, errNum)
+	Worker(taskSlice, 5, errNum, test)
 }
